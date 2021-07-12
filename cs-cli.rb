29f52237bfdad3c036535f0f7cc381c695c3588c
sammcgeown <smcgeown@vmware.com>
2021-07-12 16:42:17 +0100

    Brew formula update for code-stream-cli version v0.1.7

--- cs-cli.rb
+++ cs-cli.rb
@@ -5,25 +5,30 @@
 class CsCli < Formula
   desc "Code Stream CLI is a command line tool written in Go to interact with the vRealize Automation Code Stream APIs"
   homepage "https://github.com/vmware/code-stream-cli"
-  version "0.1.6"
+  version "0.1.7"
   license "BSD-2"
   bottle :unneeded
 
-  if OS.mac? && Hardware::CPU.intel?
-    url "https://github.com/vmware/code-stream-cli/releases/download/v0.1.6/code-stream-cli_0.1.6_Darwin_x86_64.tar.gz"
-    sha256 "cb93a5a5730b9c5a29463578662e7cd77a04688c440f517ddf9a00e1dbcd463c"
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/vmware/code-stream-cli/releases/download/v0.1.7/code-stream-cli_0.1.7_Darwin_x86_64.tar.gz"
+      sha256 "43432154aa6e002daa1adf21eda4cb46e79774b2d4428a6d490a3ea94dc4441b"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/vmware/code-stream-cli/releases/download/v0.1.7/code-stream-cli_0.1.7_Darwin_arm64.tar.gz"
+      sha256 "10b170ccde8989a848607de121f0effce68d2db0afafe738b214c3d721eb2a7d"
+    end
   end
-  if OS.mac? && Hardware::CPU.arm?
-    url "https://github.com/vmware/code-stream-cli/releases/download/v0.1.6/code-stream-cli_0.1.6_Darwin_arm64.tar.gz"
-    sha256 "a948abd5634f88fd32fecf69476a868ef0af3cc1ac238c9f21d9a77dfda488bd"
-  end
-  if OS.linux? && Hardware::CPU.intel?
-    url "https://github.com/vmware/code-stream-cli/releases/download/v0.1.6/code-stream-cli_0.1.6_Linux_x86_64.tar.gz"
-    sha256 "bde925211a2aa61ddb94584daaa7bc07a484a388ae419bf4a5b6335c912b6081"
-  end
-  if OS.linux? && Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-    url "https://github.com/vmware/code-stream-cli/releases/download/v0.1.6/code-stream-cli_0.1.6_Linux_arm64.tar.gz"
-    sha256 "1af19ff761dd15b63850c93cbe896db485509dbaec2145e6f3957e1a3ab31460"
+
+  on_linux do
+    if Hardware::CPU.intel?
+      url "https://github.com/vmware/code-stream-cli/releases/download/v0.1.7/code-stream-cli_0.1.7_Linux_x86_64.tar.gz"
+      sha256 "32ea5d9fef2c16afce652ae383a38a67359765b7f03db9968c7a5241a9b2ee9c"
+    end
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/vmware/code-stream-cli/releases/download/v0.1.7/code-stream-cli_0.1.7_Linux_arm64.tar.gz"
+      sha256 "fe9bd21745bd43250fddf1bbd788ecdce3c899d50c8610e9c568612527104861"
+    end
   end
 
   def install
